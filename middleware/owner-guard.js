e780924936ec862003ef6a9dd3af6157b0fce5d7
Lais Monteiro <lmonteiro189@gmail.com>
2020-05-22 01:00:38 +0100

    finish

--- middleware/owner-guard.js
+++ middleware/owner-guard.js
@@ -1,11 +1,11 @@
-'use strict';
-module.exports = (id) => (req, res, next) => {
-  // if (id === req.user._id) {
-  //   return true;
-  // } else {
-  //   return false;
-  // }
-};
+// 'use strict';
+// module.exports = (id) => (req, res, next) => {
+//   if (id === req.user._id) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
 // router.get('/:id/edit', routeGuard, (req, res, next) => {
 //   OwnerGuard(req.user._id);
 //   res.render('user/editProfile');
