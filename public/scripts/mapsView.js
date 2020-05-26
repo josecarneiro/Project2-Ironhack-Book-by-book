c5ccfb1609a25fc70eb6f7b8e01eea56bb57ed3e
José Carneiro <josemcarneiro@gmail.com>
2020-05-26 18:41:45 +0100

    Update mapsView.js

--- public/scripts/mapsView.js
+++ public/scripts/mapsView.js
@@ -1,27 +1,29 @@
 function initMap() {
+  /*
   let options = {
     enableHighAccuracy: true,
     timeout: 15000, // max time to wait geo
     maximumAge: 0
   };
-  window.addEventListener('load', function(event) {
-    const $latinput = document.getElementById('latitude-input').value;
-    const $lnginput = document.getElementById('longitude-input').value;
-    const lat = Number($latinput);
-    const lng = Number($lnginput);
-    const bookPosition = { lat: lat, lng: lng };
-    console.log(lat, lng);
+  */
+  const $latinput = document.getElementById('latitude-input').value;
+  const $lnginput = document.getElementById('longitude-input').value;
+  
+  const lat = Number($latinput);
+  const lng = Number($lnginput);
+  
+  const bookPosition = { lat: lat, lng: lng };
+  console.log(lat, lng);
 
-    const map = new google.maps.Map(document.getElementById('map-view'), {
-      zoom: 12,
-      center: bookPosition
-    });
+  const map = new google.maps.Map(document.getElementById('map-view'), {
+    zoom: 12,
+    center: bookPosition
+  });
 
-    const marker = new google.maps.Marker({
-      draggable: false,
-      position: bookPosition,
-      map: map,
-      animation: google.maps.Animation.DROP
-    });
+  const marker = new google.maps.Marker({
+    draggable: false,
+    position: bookPosition,
+    map: map,
+    animation: google.maps.Animation.DROP
   });
 }
